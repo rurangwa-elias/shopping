cf30df719c19e3298937ebbf06d57ca0db9ff142
Nathanael Birri <nathichuta@gmail.com>
2020-10-21 20:48:55 -0400

    added card-service microservice  to the project

---
+++ card-service/card-service/src/main/java/edu/miu/groupx/card/cardservice/CardApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.groupx.card.cardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardApplication.class, args);
+	}
+
+}
