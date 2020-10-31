f912fd42542bc0e95c3f52bc21892cbd73c2fa6c
mohammad-ah <41110875+mohammad-ah@users.noreply.github.com>
2020-10-31 02:13:21 -0500

    Update ShopController.java

--- shopping/src/main/java/edu/miu/groupx/shop/shopping/controller/ShopController.java
+++ shopping/src/main/java/edu/miu/groupx/shop/shopping/controller/ShopController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api")
 public class ShopController {
     @Autowired
     private ProductService productService;
