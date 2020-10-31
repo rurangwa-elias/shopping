6d57c81563e31a7a0a4648af9ad494acdc26d0d2
unknown <610710@cs.mum.edu>
2020-10-31 15:55:06 -0500

    report service eureka

--- report-generation-service/report-service/src/main/java/edu/miu/groupx/report/reportservice/ReportServiceApplication.java
+++ report-generation-service/report-service/src/main/java/edu/miu/groupx/report/reportservice/ReportServiceApplication.java
@@ -2,6 +2,7 @@ package edu.miu.groupx.report.reportservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 //@ComponentScan({"edu.miu.groupx.report.reportservice", "edu.miu.groupx.report.reportservice.feginClient"})
 @EnableFeignClients(basePackages = {"edu.miu.groupx.report.reportservice", "edu.miu.groupx.report.reportservice"})
 @SpringBootApplication
+@EnableEurekaClient
 public class ReportServiceApplication {
 
 	public static void main(String[] args) {
