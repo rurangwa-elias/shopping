5a75187519feb96fb795a14d5d39af0f7e956d65
mohammad-ah <41110875+mohammad-ah@users.noreply.github.com>
2020-10-31 02:14:53 -0500

    Update ShoppingApp.java

--- shopping/src/main/java/edu/miu/groupx/shop/shopping/ShoppingApp.java
+++ shopping/src/main/java/edu/miu/groupx/shop/shopping/ShoppingApp.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class ShoppingApp {
+public class ShoppingApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShoppingApp.class, args);
+        SpringApplication.run(ShoppingApplication.class, args);
     }
 
 }
